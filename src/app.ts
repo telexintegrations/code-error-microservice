b7b6d9ca0201c322832bc8522148ffa7b08a8e3f
damola <damolaadegbite0@gmail.com>
2025-03-14 09:47:47 +0100

    fix: connect to telex without errors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import requestLogger from "./middlewares/requestLogger";
 import errorRoutes from "./routes/errorRoutes";
-
+import integrationRoutes from "./routes/integrations";
+import tickRoute from "./routes/tick";
 dotenv.config();
 
 const app = express();
@@ -11,8 +13,19 @@ app.use(express.json());
 // Middleware
 app.use(requestLogger);
 
+app.use(
+  cors({
+    origin: "https://telex.im",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
+
+app.use(cors());  // Allow all origins during testing
 // Routes
 app.use("/api", errorRoutes);
+app.use(integrationRoutes);
+app.use(tickRoute);
 
 // Health Check
 app.get("/health", (_req, res) => {
