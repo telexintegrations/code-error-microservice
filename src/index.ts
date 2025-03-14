95ff69f2f8639bb35926b2f57003032a20f2bf47
chibuezemicahe <chibuezemicahe@gmail.com>
2025-03-14 10:11:40 +0100

    chore: cleanup of the file structure

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import app from "./app";
+import axios from "axios"
 
 const PORT = process.env.PORT || 4000;
+
+// const keepRenderAwake = () => {
+//     setInterval(async () => {
+//       try {
+//         const response = await axios.get(RENDER_HEALTH_CHECK_URL);
+//        console.log(`✅ Render Ping Successful: ${response.status} ${response.statusText}`);
+//       } catch (error: unknown) {
+//         if (error instanceof Error) {
+//             console.log(`❌ Render Ping Failed: ${error.message}`);
+//         } else {
+//             console.log("❌ Render Ping Failed: Unknown error");
+//         }
+//       }
+//     }, 300000); // 5 minutes
+//   };
+
 
 app.listen(PORT, () => {
   console.log(`Microservice running on port ${PORT}`);
