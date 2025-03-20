bde843689fd014c742ec35cd47d8bc56b3bd043d
JC-Coder <josephchimezie2003@gmail.com>
2025-03-20 22:37:34 +0100

    chore: add GitHub Actions workflow for deployment to server

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import "./services/zeromqService";
 import { ENV_CONFIG } from "./utils/envConfig";
 
 app.listen(ENV_CONFIG.PORT, () => {
-  console.log(`Microservice running on port ${ENV_CONFIG.PORT}`);
+  console.log(`Microservice running on http://localhost:${ENV_CONFIG.PORT}`);
 });
