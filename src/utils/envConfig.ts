e3404f3deecaabfa21ecd1949dcb40e30083029a
damola <damolaadegbite0@gmail.com>
2025-04-11 12:33:22 +0100

    chore: Hardcode WEBHOOK_URL in environment config

--- src/utils/envConfig.ts
+++ src/utils/envConfig.ts
@@ -5,6 +5,6 @@ config();
 export const ENV_CONFIG = {
   PORT: process.env.PORT || 4000,
   SERVER_URL: process.env.SERVER_URL || "http://localhost:4000",
-  WEBHOOK_URL: process.env.WEBHOOK_URL || "https://ping.telex.im/v1/webhooks",
+  WEBHOOK_URL: "https://ping.telex.im/v1/webhooks",
   GOOGLE_GENERATIVE_AI_API_KEY: process.env.GOOGLE_GENERATIVE_AI_API_KEY || "",
 };
